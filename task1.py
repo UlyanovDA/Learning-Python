2d0f8e53d7462ef40f6b9944d32739b1c39187f3
UlyanovDA <144554593+UlyanovDA@users.noreply.github.com>
2025-02-03 20:34:15 +0300

    Module 2, lab 3

---
+++ task1.py
@@ -0,0 +1,77 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+    @property
+    def name(self):
+        """Возвращает название книги"""
+        return self._name
+
+    @property
+    def author(self):
+        """Возвращает автора книги"""
+        return self._author
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        # self.name = name
+        # self.author = author
+        self._pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages):
+        if not isinstance(new_pages, int):
+            raise TypeError('Количество страниц должно быть целым числом')
+        if new_pages <= 0:
+            raise ValueError('Количество страниц должно быть положительным')
+        self._pages = new_pages
+
+    """Перегрузка __str__, __repr__ в связи с добавлением нового атрибута pages"""
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Количество страниц {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        # self.name = name
+        # self.author = author
+        self._duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration):
+        if not isinstance(new_duration, float):
+            raise TypeError('Продолжительность должна быть числом')
+        if new_duration <= 0:
+            raise ValueError('Продолжительность должна быть положительной')
+        self._duration = new_duration
+
+    """Перегрузка __str__, __repr__ в связи с добавлением нового атрибута duration"""
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Продолжительность {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
